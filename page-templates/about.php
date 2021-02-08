1201ae3d9e2a02a1caf6aa94a7342ac51f161b22
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-02-08 15:37:40 -0500

    shawn push

---
+++ page-templates/about.php
@@ -0,0 +1,19 @@
+<?php
+   /*
+    Template Name: About
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+      exit; // Exit if accessed directly.
+    }
+    ?>
+<?php get_header(); ?>
+
+<section id="page-header" class></section>
+
+<?php get_footer(); ?>
+
+
+
+<script>
+new WOW().init();
+</script>
