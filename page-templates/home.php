f92b65105a2c3acad7575cedc9faec80254e92aa
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-02-09 11:18:54 -0500

    Shawn 2/9 am push

--- page-templates/home.php
+++ page-templates/home.php
@@ -9,7 +9,21 @@
 <?php get_header(); ?>
 
 <div id="hero" class="home-hero">
-    
+    <div class="hero-wrapper container-fluid px-0">
+        <div class="hero-inner">
+            <div class="title-wrapper">
+                <h1>The<br> Future<br> of Medicine<br> Begins<br> With You</h1>
+                <div class="subtitle">We Invite You to Become Part of
+                    Something Bigger.</div>
+            </div>
+            <div class="hero-box">
+                <p>Through clinical research, we’re advancing medical science and improving the quality of life for so
+                    many people.</p>
+                <p>It start's here</p>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
