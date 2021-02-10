ce3a553840782645e3af4370e8d3e75823ece08c
Calvin-Xagency <Calvin@Xagency.io>
2021-02-10 16:04:44 -0500

    Trials Initial

--- page-templates/trials.php
+++ page-templates/trials.php
@@ -18,18 +18,40 @@
 
 <section class="container">
     <div class="row">
-        <div class="col-12 mb-5">
+        <div class="controls col-12 mb-5">
             <!-- Controls -->
-            <?php echo file_get_contents(__DIR__ . '/../img/trials1.svg'); ?>
-            <?php echo file_get_contents(__DIR__ . '/../img/trials2.svg'); ?>
-            <?php echo file_get_contents(__DIR__ . '/../img/trials3.svg'); ?>
-            <?php echo file_get_contents(__DIR__ . '/../img/trials4.svg'); ?>
-            <?php echo file_get_contents(__DIR__ . '/../img/trials5.svg'); ?>
+            <h2>Search By:</h2>
+            <div class="filter-btn">
+                <?php echo file_get_contents(__DIR__ . '/../img/trials1.svg'); ?>
+                <p>Pediatrics</p>
+            </div>
+            
+            <div class="filter-btn">
+                <?php echo file_get_contents(__DIR__ . '/../img/trials2.svg'); ?>
+                <p>Diabetes</p>
+            </div>
+
+            <div class="filter-btn">
+                <?php echo file_get_contents(__DIR__ . '/../img/trials3.svg'); ?>
+                <p>Cardiac</p>
+            </div>
+
+            <div class="filter-btn">
+                <?php echo file_get_contents(__DIR__ . '/../img/trials4.svg'); ?>
+                <p>Pulmonary</p>
+            </div>
+
+            <div class="filter-btn">
+                <?php echo file_get_contents(__DIR__ . '/../img/trials5.svg'); ?>
+                <p>Digestive</p>
+            </div>
+
         </div>
         <!-- Repeater -->
     </div>
 </section>
 
 <!-- Template Part -->
+<?php get_template_part('template-parts/cta') ?>
 
 <?php get_footer(); ?>
