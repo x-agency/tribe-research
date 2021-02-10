51180687d2ee1f0ef96dc394d413bfb8560cc946
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-02-10 15:49:09 -0500

    Cta Addition 2

--- page-templates/home.php
+++ page-templates/home.php
@@ -81,27 +81,6 @@
         </div>
     </div>
 </section>
-<section id="cta">
-<div class="container cta-wrapper">
-    <a href="" class="cta-link">
-        <div class="cta-block block-1">
-            <div class="cta-text-wrapper">
-                <div class="cta-text">Frequently Asked<br> Questions</div>
-                <div class="cta-arrow"><?php echo file_get_contents(__DIR__ . '/../img/link-arrow.svg'); ?></div>
-            </div>
-        </div>
-    </a>
-    
-    <a href="" class="cta-link">
-        <div class="cta-block block-2">
-            <div class="cta-text-wrapper">
-                <div class="cta-text">Ready To?<br> Get Started?</div>
-                <div class="cta-arrow"><?php echo file_get_contents(__DIR__ . '/../img/link-arrow.svg'); ?></div>
-            </div>
-        </div>
-    </a>
-    
-</div>
-</section>
 
+<?php get_template_part('template-parts/cta') ?>
 <?php get_footer(); ?>
