1eed294c17cf79ebffd04adae3006355cba20dd5
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-02-10 13:47:07 -0500

    gitignore update

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -12,7 +12,7 @@
 // Local project URL of your already running WordPress site.
 // > Could be something like "wpgulp.local" or "localhost"
 // > depending upon your local WordPress setup.
-const projectURL = 'localhost:10018';
+const projectURL = 'localhost:10028';
 
 // Theme/Plugin URL. Leave it like it is; since our gulpfile.js lives in the root folder.
 const productURL = './';
