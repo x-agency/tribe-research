92505e4f3bff60e450d8c13804a13b9e971f5d47
Calvin-Xagency <Calvin@Xagency.io>
2021-02-11 14:25:29 -0500

    Create contact.php

---
+++ page-templates/contact.php
@@ -0,0 +1,14 @@
+<?php
+   /*
+    Template Name: Sponsors
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+      exit; // Exit if accessed directly.
+    }
+    ?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
+
+
+
