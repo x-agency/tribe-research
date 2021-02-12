f13da557bbb14450742d409520778a040b40e020
Calvin-Xagency <Calvin@Xagency.io>
2021-02-12 10:40:45 -0500

    Update Sponsors

--- page-templates/sponsors.php
+++ page-templates/sponsors.php
@@ -9,13 +9,179 @@
 <?php get_header(); ?>
 
 <section class="hero hero-overlap">
-    <img class="bg" src="/wp-content/themes/tribe-research/img/about-hero.jpg">    
+    <img class="bg" src="/wp-content/themes/tribe-research/img/sponsors-hero.jpg">    
     <div class="content">
-        <img src="/wp-content/themes/tribe-research/img/doctor.jpg" alt="Female doctor with hand on shoulder of female patient.">
+        <img src="/wp-content/themes/tribe-research/img/sponsors.jpg" alt="Female doctor with hand on shoulder of female patient.">
         <div class="text">
-            <img src="/wp-content/themes/tribe-research/img/about-text.png"/>
-            <h1>TRIBE CLINICAL RESEARCH</h1>
+            <img src="/wp-content/themes/tribe-research/img/sponsors-text.png"/>
+            <p>We’re building a collaborative network in the upstate based on shared principles of respect and commitment to improve patient care through quality clinical research. This continued dedication to solid partnerships assists in providing quality outcomes for clinical trials while providing the best possible medical care to our study volunteers. If you are interested in partnering with us to offer clinical research at your practice, please reach out to us. Together we can make a huge impact in the filed of medicine.</p>
         </div>    
+    </div>
+</section>
+
+<section class="explore container">
+    <div class="row">
+        <div class="controls col-12 my-5">
+            <h2>Explore</h2>
+                <div class="filter">
+                    <div class="toggle-container"><div class="filter-toggle"></div></div>
+                    <div class="btn-alt">
+                        <a href="#our-site">
+                            <p>Our Site</p>
+                        </a>
+                    </div>
+                    <div class="btn-alt">
+                        <a href="#monitor-guidelines">
+                            <p>Monitor Guidelines</p>
+                        </a>
+                    </div>
+                    <div class="btn-alt">
+                        <a href="#daily-schedule">
+                            <p>Daily Schedule</p>
+                        </a>
+                    </div>
+                    <div class="btn-alt">
+                        <a href="#our-area">
+                            <p>Our Area</p>
+                        </a>
+                    </div>
+                    <div class="btn-alt">
+                        <a href="#hotels-restaurants">
+                            <p>Restaurants/Hotels</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="anchor" id="our-site"></div>
+        <div>
+            <p class="title"></p>
+            <ul class="col-lg-6">
+                <li>Easy access to a major airport</li>
+                <li>Designated monitoring space with private space available at your request</li>
+                <li>Telephone for monitor use</li>
+                <li>High speed wireless DSL</li>
+                <li>High volume copier</li>
+            </ul>
+            <ul class="col-lg-6">
+                <li>Shredder</li>
+                <li>Fax machine</li>
+                <li>On site storage of all documents</li>
+                <li>Low query rates with rapid resolution</li>
+                <li>Full time Data/Quality Assurance Coordinator</li>
+            </ul>
+            <a href="" class="btn">Take Our Virtual Tour</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="testimonial">
+            <div class="quote"></div>
+            <p>I love your site and I wish it was mine. Monitors often tell sites only what they are doing wrong so I wanted to make sure I told you that you are are doing a great job!</p>
+            <p class="author">Monitor</p>
+            <p class="source">Tribe Clinical Research</p>
+        </div>
+    </div>
+</section>
+
+<section class="monitor">
+    <div class="section-hero">
+        <div class="anchor" id="monitor-guidelines"></div>
+        <img class="bg" src="/wp-content/themes/tribe-research/img/sponsors-monitor-hero.jpg">
+        <img src="/wp-content/themes/tribe-research/img/monitor-text.png" alt="Monitor Guidelines">
+        <div class="line"></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <p>We look forward to working with you and hope these guidelines ensure a more efficient use of your time and ours during your monitoring visit. Due to our current study volume and increased patient visit schedule we ask that you adhere to the following guidelines.</p>
+            <ul>
+                <li><span>Please try and schedule your next RMV at each visit.</span>This will allow us to adjust our schedule accordingly and give you adequate time during your visit with us. Also, please give us advance warning if you will be bringing a co-CRA or manager with you so we can accommodate everyone comfortably.</li>
+                <li><span>The Quality Assurance Coordinator is considered a monitor liaison.</span>If you have any questions please direct them to him/her. The Quality Assurance Coordinator will take care of all CRF corrections, and scheduling of monitoring visits. If needed the Quality Assurance Coordinator will delegate source corrections to the appropriate medical staff member.</li>
+                <li><span>The Regulatory Coordinator is responsible for Regulatory and IRB questions.</span>(e.g. Site Delegation Logs, Enrollment Logs, & etc.). She/he will also be available to assist you.</li>
+                <li><span>We ask that Monitors do not walk to the back offices or exam rooms</span>as we want to maintain privacy for our patients. If you need to talk with a study coordinator, please ask the front office for assistance.</li>
+                <li><span>For your convenience, there is a fax/copier machine available to you</span>located in the front office. You are welcome to utilize this machine as needed, but we do ask to please keep copying to a minimum and to only make copies concerning our site.</li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="anchor" id="daily-schedule"></div>
+            <div>
+                <h2></h2>
+                <div>
+                    <div class="time">
+                        <p><span>Monitoring Visit:</span></p>
+                        <p>8:30am - 4:30pm</p>
+                        <p>(Monday-Thursday)</p>
+                    </div>
+                    <div class="vseparator"></div>
+                    <div class="time">
+                        <p><span>Medical Director:</span></p>
+                        <p>11:00am - 1:00pm</p>
+                        <p>(Monday, Wednesday, Thursday)</p>
+                    </div>
+                </div>
+                <p><span>Coordinators assigned to your study will be available after 2:00 p.m.</span></p>
+                <a href="#" class="btn">Download Monitor Guidelines</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="our-area">
+    <div class="section-hero">
+        <div class="anchor" id="our-area"></div>
+        <div class="background"><img src="/wp-content/themes/tribe-research/img/sponsors-area-hero.jpg"></div>
+        <div class="foreground">
+            <div>
+                <h2 class="alt">Our Area</h2>
+                <p>We would like to share some information to help you enjoy your stay in the area. Our site is easily accessible from Greer or Greenville. If you find a favorite hotel or restaurant, please let us know so that we can share the information with other CRA’s working at our site. We hope you find this information helpful and will love the area as much as we do!</p>
+                <a href="#" class="link arrow">Directions</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="anchor" id="hotels-restaurants"></div>
+            <div class="controls col-12 my-5">
+                <h2>Search Area By:</h2>
+                    <div class="filter">
+                        <div class="toggle-container"><div class="filter-toggle"></div></div>
+                        <div class="filter-btn">
+                            <?php echo file_get_contents(__DIR__ . '/../img/trials1.svg'); ?>
+                            <p>Hotels</p>
+                        </div>
+                        <div class="filter-btn">
+                            <?php echo file_get_contents(__DIR__ . '/../img/trials1.svg'); ?>
+                            <p>Restaurants</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>I-85/Pelham Road Area</h2>
+                <?php for($i = 0; $i < 8; $i++) : ?>
+                    <div class="area">
+                        <a href="#" class="link arrow">Directions</a>
+                        <p class="road">5009 Pelham Road</p>
+                        <p class="city">Greenville, SC 29615</p>
+                        <p class="number">(864) 297-5353</p>
+                    </div>
+                <?php endfor; ?> 
+            </div>
+            <div class="col-lg-6">
+                <h2>I-85/Pelham Road Area</h2>
+                <?php for($i = 0; $i < 4; $i++) : ?>
+                    <div class="area">
+                        <a href="#" class="link arrow">Directions</a>
+                        <p class="road">5009 Pelham Road</p>
+                        <p class="city">Greenville, SC 29615</p>
+                        <p class="number">(864) 297-5353</p>
+                    </div>
+                <?php endfor; ?> 
+            </div>
+        </div>
     </div>
 </section>
 
