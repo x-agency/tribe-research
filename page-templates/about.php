0a5e3f5e6998df93ec5ce6ff8818e8251c080f49
Calvin-Xagency <Calvin@Xagency.io>
2021-04-09 11:14:18 -0400

    Update copy

--- page-templates/about.php
+++ page-templates/about.php
@@ -24,12 +24,12 @@
     <div class="row">
         <div class="col-12 mb-3"><h2 class="title">The Tribe Difference</h1></div>
         <div class="col-lg-6">
-            <p>The health and quality of life of millions has been improved because of advances in science and technology, and the willingness of individuals like you to take part in clinical research. The role of our study volunteers in clinical research is crucial in the quest for knowledge that will improve health and provide hope for future generations. With your help, the research studies at Tribe Clinical Research will help shape the future of medicine.</p>
-            <p>Become a study volunteer to learn more about your condition, get access to new medications and treatments, and help further medical research for the entire healthcare community. During a clinical trial, the study volunteer will meet with a board-certified physician and receive one on one medical care. There is no cost to the study volunteer, in fact, most clinical trials study volunteers are compensated for their time and participation while in the study.</p>
+            <p>The health and quality of life of millions have been improved because of advances in science and technology, and the willingness of individuals like you to take part in clinical research. Our study volunteers’ role is crucial in the quest for knowledge that will improve health and provide hope for future generations. With your help, the research studies at Tribe Clinical Research will help shape the future of medicine.</p>
+            <p>Become a study volunteer to learn more about your condition, get access to new medications and treatments, and help further medical research for the entire healthcare community. During a clinical trial, the study volunteer will meet with a board-certified physician and receive one on one medical care. There is no cost to the study volunteer, and most clinical trials study volunteers are compensated for their time and participation while in the study.</p>
         </div>
         <div class="col-lg-6">
-            <p>We have managed over 100 phase II to IV clinical trials in partnership with over 20 pharmaceutical companies. Studies conducted include: Diabetes Type 1, Diabetes Type 2, COPD, Asthma and Allergies, Irritable Bowel Syndrome, GERD, Inflammatory Bowel Disease, Hepatitis C, Osteoarthritis, Osteoporosis, Hypertension, Hyperlipidemia, Adolescent Migraine, Adult Migraine, Obesity, Vaccinations and Acute Coronary Syndrome.</p>
-            <p>Tribe Clinical Research is recognized for excellent patient care, outstanding patient enrollment and quality data management. We continually exceed standards of Good Clinical Practices.</p>
+            <p>We have managed over 100 phase II to IV clinical trials in partnership with over 20 pharmaceutical companies. Studies conducted include Diabetes Type 1, Diabetes Type 2, COPD, Asthma and Allergies, Irritable Bowel Syndrome, GERD, Inflammatory Bowel Disease, Hepatitis C, Osteoarthritis, Osteoporosis, Hypertension, Hyperlipidemia, Adolescent Migraine, Adult Migraine, Obesity, Vaccinations, and Acute Coronary Syndrome.</p>
+            <p>Tribe Clinical Research is recognized for excellent patient care, outstanding patient enrollment, and quality data management. We continually exceed standards of Good Clinical Practices.</p>
         </div>
     </div>
 </section>
@@ -67,8 +67,8 @@
             <h2>Our Team:</h2>
             <h2 class="alt">Tested & Proven</h2>
             <p>Our clinical studies are administered by a team of healthcare professionals (physicians and nurse practitioners) who have been involved in many successful research projects.</p>
-            <p>We operate with a groundbreaking approach—partnering with primary care physicians in order to provide a better continuum of care for our patient volunteers.</p>
-            <p>Tribe Clinical Research is recognized for excellent patient care, outstanding patient enrollment and quality data management. We continually exceed standards of Good Clinical Practices.</p>
+            <p>We operate with a groundbreaking approach—partnering with primary care physicians to provide a better continuum of care for our patient volunteers.</p>
+            <p>Tribe Clinical Research is recognized for excellent patient care, outstanding patient enrollment, and quality data management. We continually exceed standards of Good Clinical Practices.</p>
         </div>
     </div>
 </section>
